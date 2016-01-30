afb327627aa3083026933c0807e42e137bb40f84
Emilia <szymanska.emilka@gmail.com>
2016-01-30 23:21:24 +0100

    Room management finished

--- app/roomsManager.js
+++ app/roomsManager.js
@@ -5,14 +5,14 @@ var _ = require('lodash');
 class RoomManager {
     constructor(roomsNo, usersInRoomNo) {
         this.roomsNumber = roomsNo;
-        this.maxUsersInRoom= usersInRoomNo;
+        this.maxUsersInRoom = usersInRoomNo;
         this.rooms = this.initRooms();
     }
-    
+
     get roomsNo() {
         return this.roomsNumber;
     }
-    
+
     get roomsList() {
         let result = []
         _.each(this.rooms, room => {
@@ -25,34 +25,52 @@ class RoomManager {
         });
         return result;
     }
-    
+
     initRooms() {
         var rooms = [];
         _.times(this.roomsNumber, (i) => {
             rooms.push({
                 id: i,
-                name: 'Room'+i,
+                name: 'Room' + i,
                 users: []
             });
         });
         return rooms;
     }
-    
-    joinRoom(roomId, clientId) {
-        if(this.rooms[roomId].users.length == this.maxUsersInRoom){
-            return false;
+
+    joinRoom(roomId, clientId, username) {
+        if (roomId < 0 || roomId >= this.roomsNo)
+            return 'Wrong room id!';
+        if (this.rooms[roomId].users.length == this.maxUsersInRoom) {
+            return 'Room is full!';
         } else {
-            this.rooms[roomId].users.push(clientId);
+            this.rooms[roomId].users.push({
+                username: username,
+                id: clientId,
+                isReady: true
+            });
+            return null;
         }
     }
-    
+
     leaveRoom(roomId, clientId) {
-        _.pull(this.rooms[roomId].users, clientId);
+        _.remove(this.rooms[roomId].users, { id: clientId});
     }
-    
+
     usersInRoomCount(roomId) {
         return this.rooms[roomId].users.length;
-    }   
+    }
+    
+    roomIsReady(roomId) {
+        return _.every(this.roomsList[roomId].users, { isReady: true});
+    }
+    
+    setAsReady(roomId, clientId) {
+        var user = _.find(this.roomsList[roomId].users, { id: clientId});
+        if(user){
+            user.isReady = true;
+        }
+    }
 }
 
 module.exports = RoomManager;
--- app/socketServer.js
+++ app/socketServer.js
@@ -21,41 +21,73 @@ module.exports.init = (http) => {
             onDisconnect(socket);
         });
 
-        socket.on('chat message', msg =>{
-             onChatMessage(socket, msg);
+        socket.on('chat message', msg => {
+            onChatMessage(socket, msg);
+        });
+
+        socket.on('join room', id => {
+            onJoinRoom(id, socket);
+        });
+
+        socket.on('leave room', () => {
+            onLeaveRoom(socket);
+        })
+
+        socket.on('ready', () => {
+            onReady(socket);
         })
     });
+}
 
-    var connectUser = () => {
-        console.log('a user connected');
-        connectedUsers++;
-        io.emit('users', connectedUsers);
-    }
 
-    var onLogIn = (socket, username) => {
+var connectUser = () => {
+    console.log('a user connected');
+    connectedUsers++;
+    io.emit('users', connectedUsers);
+}
+
+var onLogIn = (socket, username) => {
+    if (!socket.username) {
         console.log('User logged in: ' + username);
         socket.username = username;
         socket.join(lobbyChannel);
+        socket.channel = lobbyChannel;
         io.to(socket.id).emit('update rooms', roomManager.roomsList);
         io.to(lobbyChannel).emit('join lobby', username);
     }
-
-    var onDisconnect = (socket) => {
-        console.log('user disconnected');
-        connectedUsers--;
-        io.to(lobbyChannel).emit('leave lobby', socket.username);
-    }
-
-    var onChatMessage = (socket, msg) => {
-        console.log('message: ' + msg);
-        console.log({
-            username: socket.username,
-            text: msg
-        })
-        io.to(lobbyChannel).emit('chat message', {
-            nickname: socket.username,
-            text: msg
-        });
-    }
 }
 
+var onDisconnect = (socket) => {
+    console.log('user disconnected');
+    connectedUsers--;
+    io.to(socket.channel).emit('leave lobby', socket.username);
+}
+
+var onChatMessage = (socket, msg) => {
+    console.log(socket.username + ': ' + msg + ' from: ' + socket.channel);
+    socket.broadcast.to(socket.channel).emit('chat message', {
+        nickname: socket.username,
+        text: msg
+    });
+}
+
+var onJoinRoom = (id, socket) => {
+    let res = roomManager.joinRoom(id, socket.id, socket.username);
+    socket.join(id);
+    socket.channel = id;
+    io.to(socket.id).emit('game init', res || 'Connected successfully');
+    socket.broadcast.to(id).emit('joined room', socket.username);
+    io.emit('update rooms', roomManager.roomsList);
+    // TODO: collect game data and send it back    
+}
+
+var onLeaveRoom = (socket) => {
+    roomManager.leaveRoom(socket.channel, socket.id);
+    socket.broadcast.to(socket.channel).emit('user left', socket.username);
+    socket.channel = lobbyChannel;
+    io.emit('update rooms', roomManager.roomsList);
+}
+
+var onReady = socket => {
+    
+}
